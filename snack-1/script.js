2c7a5b42b26ed1f96ff4341f64407da9c2a74484
xhonatankonomi <xhoni8962@gmail.com>
2025-10-22 15:28:49 +0200

    feat: Add forEach loop to print names
    This commit introduces a `forEach` loop to iterate over the `names` array and print each element to the console. This provides an alternative approach to the existing `for` loop, demonstrating another way to accomplish the same task.

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,12 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+for (let i = 0; i < names.length; i++) {
+    const element = names[i]
+    console.log(element)
+}
+
+names.forEach((element) => {
+    console.log(element)
+})
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
