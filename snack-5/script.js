cd0f032629d3c41428de2e598fc92386b453209b
xhonatankonomi <xhoni8962@gmail.com>
2025-10-22 16:16:34 +0200

    Refactor script.js to use filter() instead of for loop
    This commit message follows the conventional commit format by using square brackets and a descriptive title. It succinctly summarizes that the script.js was refactored to replace the manual loop with the built-in filter() method, improving readability and performance. The commit is clear and informative in describing what changes were made and why they were implemented.

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,32 @@
 const nums = [2, 8, 4, 7, 12, 87];
+let pari = [];
 
+for (i = 0; i < nums.length; i++) {
+    const numeri = nums[i]
+    if (numeri % 2 == 0) {
+        pari.push(numeri)
+    }
+}
+
+console.log(pari)
+
+const paro = nums.filter((numbers) => {
+    if (numbers % 2 == 0) {
+        return true
+    }
+    return false;
+})
+
+console.log(paro)
+
+nums.forEach((element) => {
+  const numeri = nums[i]
+    if (numeri % 2 == 0) {
+        pari.push(numeri)
+    }
+}
+)
+
+console.log(pari)
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
