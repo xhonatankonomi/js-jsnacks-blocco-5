19944ca973924728132536d0f8662454412521b8
xhonatankonomi <xhoni8962@gmail.com>
2025-10-22 16:09:52 +0200

    feat: extract authors from posts array using multiple methods
    The commit introduces three new code snippets that extract author names from the posts array. The first snippet uses a loop to push each post's author into an autori array and prints it. The second snippet employs the forEach method on posts, pushing each author into autori2 and logging the result. The third snippet applies the map function to posts, creating a filtro array with each author and outputting the contents.
    
    This commit improves code readability by demonstrating different looping mechanisms for extracting authors from an array, while maintaining clarity and functionality in the script.js file.

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,26 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+
+let autori = [];
+let autori2 = [];
+let autori3 = [];
+
+for (let i = 0; i < posts.length; i++) {
+    const element = posts[i];
+    autori.push(element.author)
+}
+console.log(autori)
+
+posts.forEach((element1) => {
+  autori2.push(element1.author)
+})
+console.log(autori2)
+
+const filtro = posts.map((element2) => {
+  return element2.author
+})
+console.log(filtro)
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
