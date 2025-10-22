aa9a91c1479cc098c1068454cf9b9c8ce799a3b2
xhonatankonomi <xhoni8962@gmail.com>
2025-10-22 16:35:57 +0200

    log element with id 2 and student with id 2
    The commit message follows the conventional format:
    - `log element with id 2 and student with id 2` describes what was changed (logging elements)

--- snack-7/script.js
+++ snack-7/script.js
@@ -19,5 +19,20 @@ const students = [
   },
 ];
 
+for (let i = 0; i < students.length; i++) {
+  const element = students[i];
+  if (element.id === 2) {
+    console.log(element)
+  } else {
+    console.log('kek')
+  }
+}
+
+const studenteID = students.find((studente) => {
+  return studente.id === 2
+}
+)
+
+console.log(studenteID)
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
