705a72420b3848d24ebbf5e0cf767cb5b5adb430
xhonatankonomi <xhoni8962@gmail.com>
2025-10-22 15:58:37 +0200

    Incremental and map functions to add 1 to each number in an array
    This commit message follows the conventional commit format:
    - Short title (72 characters)
    - Describes what was changed (incrementing numbers using for loop and map function)
    - Clearly explains why it was done (to create a new array with incremented values)

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,17 @@
 const numbers = [2, 8, 4, 7, 2, 87];
+let numeriSomma = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    numeriSomma.push(element + 1);
+}
+console.log(numeriSomma)
+
+const somma = numbers.map((numero) => {
+    return numero + 1;
+})
+
+console.log(somma)
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
